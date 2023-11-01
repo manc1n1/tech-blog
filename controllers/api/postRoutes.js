7c049b81fad2468e08feb6d8c066d5aa00c9bc2f
manc1n1 <mancinij1111@gmail.com>
2023-10-31 22:18:01 -0400

    Update postRoutes.js

--- controllers/api/postRoutes.js
+++ controllers/api/postRoutes.js
@@ -2,7 +2,7 @@ const router = require('express').Router();
 const { Post } = require('../../models');
 const withAuth = require('../../utils/auth');
 
-router.post('/create/:id', withAuth, async (req, res) => {
+router.post('/create', withAuth, async (req, res) => {
 	try {
 		const newPost = await Post.create({
 			...req.body,
